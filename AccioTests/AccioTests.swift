45cbc44980771527f0e1029d7b20324baca9e907
Björn Saemundsson <bjornorri@gmail.com>
2025-12-14 13:48:44 +0100

    Initial Commit

---
+++ AccioTests/AccioTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccioTests.swift
+//  AccioTests
+//
+//  Created by Bjorn Orri Saemundsson on 14.12.2025.
+//
+
+import Testing
+@testable import Accio
+
+struct AccioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
