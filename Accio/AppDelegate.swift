11318adfbff8817c700abc2d51f3d23c7194fbe7
Björn Orri <bjornorri@gmail.com>
2025-12-28 13:08:56 +0000

    Add settings versioning keys for future migrations
    Add settingsSchemaVersion and lastKnownAppVersion keys to support
    future settings migrations.

--- Accio/AppDelegate.swift
+++ Accio/AppDelegate.swift
@@ -67,5 +67,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         openSettings()
         return false
     }
+
 }
 
--- Accio/Core/DefaultsKeys.swift
+++ Accio/Core/DefaultsKeys.swift
@@ -7,9 +7,21 @@
 
 import Defaults
 
+/// Current settings schema version. Increment when making breaking changes to settings structure.
+let currentSettingsSchemaVersion = 1
+
 // MARK: - Defaults Keys
 
 extension Defaults.Keys {
+    /// Settings schema version for migrations
+    static let settingsSchemaVersion = Key<Int>(
+        "settingsSchemaVersion",
+        default: currentSettingsSchemaVersion
+    )
+
+    /// App version when settings were last saved (for migration purposes)
+    static let lastKnownAppVersion = Key<String?>("lastKnownAppVersion")
+
     /// Global behavior settings for all hotkey bindings
     static let appBehaviorSettings = Key<AppBehaviorSettings>(
         "appBehaviorSettings",
